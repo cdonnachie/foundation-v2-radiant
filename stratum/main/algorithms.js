201d9847c77c5c091e7558895ab10bf8eb35137b
Craig Donnachie <88741883+cdonnachie@users.noreply.github.com>
2023-05-08 12:16:17 -0400

    Add sha256d to algorithms.js

--- stratum/main/algorithms.js
+++ stratum/main/algorithms.js
@@ -15,6 +15,17 @@ const Algorithms = {
       };
     }
   },
+
+  // Sha256d Algorithm
+  'sha256d': {
+    multiplier: 1,
+    diff: parseInt('0x00000000ffff0000000000000000000000000000000000000000000000000000'),
+    hash: function() {
+      return function() {
+        return hashing.sha256d.apply(this, arguments);
+      };
+    }
+  },  
 };
 
 module.exports = Algorithms;
